035af1a525db8c3ba56a1699808c1199e0e3d4d9
nprez <nick_prezioso@hotmail.com>
2017-10-23 18:12:02 -0400

    some nonsense written; learning python

--- sock352.py
+++ sock352.py
@@ -8,39 +8,52 @@ import sys
 # define the UDP ports all messages are sent
 # and received from
 
-def init(UDPportTx,UDPportRx):   # initialize your UDP socket here 
-    pass 
+sendPort = None
+rcvPort = None
+
+def init(UDPportTx,UDPportRx):   # initialize your UDP socket here
+    if(UDPportTx == None or UDPportTx == 0):
+        sendPort = 27182
+    else:
+        sendPort = UDPportTx
+
+    if(UDPportRx == None or UDPportRx == 0):
+        rcvPort = 27182
+    else:
+        rcvPort = UDPportRx
+
+    pass
     
 class socket:
-    
-    def __init__(self):  # fill in your code here 
-        return
-    
-    def bind(self,address):
-        return 
 
-    def connect(self,address):  # fill in your code here 
-        return 
+    def __init__(self):  # fill in your code here
+        return {
+            "sPort": sendPort,
+            "rPort": rcvPort,
+            "addr": None
+        }
+
+    def bind(self,address):
+        return
+
+    def connect(self,address):  # fill in your code here
+        self.addr = address;
+        return
     
     def listen(self,backlog):
         return
 
     def accept(self):
-        (clientsocket, address) = (1,1)  # change this to your code 
+        (clientsocket, address) = (1,1)  # change this to your code
         return (clientsocket,address)
     
-    def close(self):   # fill in your code here 
-        return 
+    def close(self):   # fill in your code here
+        return
 
     def send(self,buffer):
-        bytessent = 0     # fill in your code here 
+        bytessent = 0     # fill in your code here
         return bytesent 
 
     def recv(self,nbytes):
         bytesreceived = 0     # fill in your code here
         return bytesreceived 
-
-
-    
-
-
